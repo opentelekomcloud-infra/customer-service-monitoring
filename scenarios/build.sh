e75d29609fc3e02dc574e85c27772da97c9c48bb
Anton Kachurin <katchuring@gmail.com>
2019-08-08 21:59:14 +0300

    Structure updated, cleaned up bash scripts

--- scenarios/build.sh
+++ scenarios/build.sh
@@ -2,18 +2,17 @@
 
 scenario_name="scenario$1"
 
-project_root=$( sh ./get_project_root.sh )
+project_root=$(sh ./core/get_project_root.sh)
 pre_build="pre_build.sh"
 post_build="post_build.sh"
 
-
-# first — build infra
+# first - build infra
 terraform_dir="${project_root}/scenarios/${scenario_name}"
-cd ${terraform_dir}
-if [[ -z ${pre_build} ]]; then bash ${pre_build}; fi
+cd "${terraform_dir}" || exit 1
+if [[ -e ${pre_build} ]]; then bash ${pre_build}; fi
 terraform apply -auto-approve -input=false
-if [[ -z ${post_build} ]]; then bash ${post_build}; fi  # here goes setting up env variables
+if [[ -e ${post_build} ]]; then bash ${post_build}; fi # here goes setting up env variables
 
-# second — configure build infra
-cd ${project_root}
+# second - configure build infra
+cd "${project_root}" || exit 1
 ansible-playbook -i "inventory/prod" "playbooks/${scenario_name}_setup.yml"
--- scenarios/skeleton.sh
+++ scenarios/skeleton.sh
@@ -2,21 +2,30 @@
 # Build skeleton for new scenario
 
 if [[ -z $1 ]]; then
-    echo "Scenario ID is missing"
+  echo "Scenario ID is missing"
+  exit 1
 fi
 
 target_name="$1"
-project_root=$( sh ./get_project_root.sh )
+project_root=$(sh ./core/get_project_root.sh)
 
-cd ${project_root}
+cd "${project_root}" || exit 2
 scn_folder="${project_root}/scenarios/${target_name}"
-mkdir -p ${scn_folder}
+mkdir -p "${scn_folder}"
 
-shebang="#!/usr/bin/env bash"
-pre_b="${scn_folder}/pre_build.sh"
-if [[ ! -e ${pre_b} ]]; then echo "${shebang}" > "${pre_b}"; fi
-post_b="${scn_folder}/post_build.sh"
-if [[ ! -e "${post_b}" ]]; then echo "${shebang}" > "${post_b}"; fi
+shebang="#!/usr/bin/env bash\n"
+
+function init_if_missing() {
+    f_path="${scn_folder}/$1"
+    data="$2"
+    if [[ ! -e ${f_path} ]]; then
+        echo "${data}" > "${f_path}"
+    fi
+    echo "File ${f_path} initialized"
+}
+
+init_if_missing "pre_build.sh" "${shebang}"
+init_if_missing "post_build.sh" "${shebang}"
 
 tf_template="terraform {
   required_providers {
@@ -36,8 +45,7 @@ provider \"opentelekomcloud\" {
   auth_url = \"https://iam.eu-de.otc.t-systems.com:443/v3\"
 }
 "
-infra_tf="${scn_folder}/infrastructure.tf"
-if [[ ! -e "${infra_tf}" ]]; then echo "${shebang}" > "${post_b}"; fi
-echo "${tf_template}" > "${infra_tf}"
+init_if_missing "infrastructure.tf" "${tf_template}"
+terraform init "${scn_folder}"
 
 touch "${project_root}/playbooks/scenarios/${target_name}_setup.yml"
