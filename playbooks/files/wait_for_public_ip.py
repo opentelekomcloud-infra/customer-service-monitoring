3870e6c35a807ae8d4a8bf8b7b21fb53394d1039
Anton Kachurin <anton.kachurin@t-systems.com>
2019-08-16 18:10:23 +0300

    Fixing scenario2 asnible playbook

--- playbooks/files/wait_for_public_ip.py
+++ playbooks/files/wait_for_public_ip.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """Check availability with given public ip on port 22"""
 import socket
 import sys
@@ -14,10 +15,7 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         raise AttributeError("No positional arguments given to wait script")
     address = sys.argv[1]
-    timeout_seconds = "60"
+    timeout_seconds = 120
     if len(sys.argv) > 2 and sys.argv[2].isdigit():
-        timeout_seconds = sys.argv[2]
-    timeout_seconds = int(timeout_seconds)
-    if not timeout_seconds.isdigit():
-        timeout_seconds = 120 # 2 minutes of waiting by default
+        timeout_seconds = int(sys.argv[2])
     main(address, timeout_seconds)
