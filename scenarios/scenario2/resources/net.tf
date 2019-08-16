2ffcdbe214ccbe2a89dd79866e553f8e5c1daf59
Anton Kachurin <anton.kachurin@t-systems.com>
2019-08-16 18:40:48 +0300

    Add dns to scn2 subnet

--- scenarios/scenario2/resources/net.tf
+++ scenarios/scenario2/resources/net.tf
@@ -8,6 +8,7 @@ resource "opentelekomcloud_vpc_subnet_v1" "subnet" {
   cidr = "${var.net_address}.0/24"
   gateway_ip = "${var.net_address}.1"
   vpc_id = opentelekomcloud_vpc_v1.vpc.id
+  primary_dns = "8.8.8.8"
   depends_on = [
     opentelekomcloud_vpc_v1.vpc
   ]
