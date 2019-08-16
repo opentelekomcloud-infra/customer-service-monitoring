38fc97da22d9041eeb18694faf9302993cf753cf
Anton Kachurin <anton.kachurin@t-systems.com>
2019-08-16 18:32:45 +0300

    Get rid of quotes everywhere in post_build
    Add post_build generation from template in skeleton.sh

--- scenarios/core/post_build_template.sh
+++ scenarios/core/post_build_template.sh
@@ -5,8 +5,3 @@ function substr() {
     var_name=$1
     echo $( echo "${output}" | grep -E "${var_name} =" | grep -oE "\"(.+)\"" | sed -e 's/^"//' -e 's/"$//' )
 }
-
-export PSQL_PASSWORD=$( substr "db_password" )
-export PSQL_USERNAME=$( substr "db_username" )
-export PSQL_ADDRESS=$( substr "db_address" )
-export SERVER_PUBLIC_IP=$( substr "scn2_public_ip" )
