cdfd513f8057ec2d275f80ea15af4a86319bbfbc
Anton Kachurin <anton.kachurin@t-systems.com>
2019-08-20 10:07:36 +0300

    Feature/scenario2 (#9)
    * Initial commit of scenario #2 scratch
    
    * Fix lint error
    
    * Added .venv to gitignore
    
    * Minor changes to project setup
    
    * Configured role for remote server
    
    * Included ansible otc modules as library
    
    * Included ansible otc modules as library
    
    * Added missing library -> otc modules link
    
    * Add openstacksdk to dependencies
    
    * Fixing issues with scn2 playbook
    
    * Minor changes
    
    * Update .gitmodules
    
    Removed redundant submodule
    
    * Added otcextensions dependency
    
    * Removed duplicating .venv in .gitignore
    
    * Using terraform to create infrastructure now
    
    * Introduce terraform + scenario skeleton generation
    
    * Removed `scenario` prefix from generated skeletons
    
    * Added terraform template to .gitignore
    
    * Updated infrastructure for scenario2
    
    * Updated ansible playbook for scenario2 setup
    
    * Restructured scenario2
    
    Added RDS creation
    
    * Restructured scenario2
    
    Added RDS creation
    
    * Fixing issues after merge with master
    
    * Fixed scenario2_setup playbook to match new approach
    
    * Minor fixes for terraform config for scenario2
    
    * Intermediate commit
    
    Fixes in build script
    
    * Removed useless playbook
    
    * Minor change in build script
    
    * Minor fixes
    
    * Fixes in env variables
    
    * Chmod +x for build and skeleton scripts
    
    * Add wait for public IP before usage
    
    * Fix not founding [[ in get_project_root.sh
    
    * Fixing scenario2 asnible playbook
    
    * Fixing scenario2 asnible playbook
    
    * Get rid of quotes in address
    
    * Get rid of quotes everywhere in post_build
    
    Add post_build generation from template in skeleton.sh
    
    * Add dns to scn2 subnet
    
    * Add update cache before py36 install
    
    * Scenario2 playbook fixes
    
    * Changed config dir mode
    
    * TSS module name change
    
    * Reverted pyproject.toml
    
    * Delete poetry.lock
    
    * Update requirements.yml
    
    * Minor change in post-build template
    
    Changed description and name of function for getting variable value from Terraform output
    
    * Update skeleton.sh
    
    Micro update — replace `cat` with `<`
    
    * Server config template was working incorrectly

--- scenarios/core/get_project_root.sh
+++ scenarios/core/get_project_root.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 MY_PATH="$(cd "$(dirname "$0")/../.." && pwd)"
-if [[ -z "$MY_PATH" ]]; then
-  # error; for some reason, the path is not accessible
-  # to the script (e.g. permissions re-evaled after suid)
-  exit 1 # fail
-fi
 echo "$MY_PATH"
---
+++ scenarios/scenario2/resources/net.tf
@@ -0,0 +1,20 @@
+resource "opentelekomcloud_vpc_v1" "vpc" {
+  name = "net_${var.postfix}"
+  cidr = "${var.net_address}.0/16"
+}
+
+resource "opentelekomcloud_vpc_subnet_v1" "subnet" {
+  name = "subnet_${var.postfix}"
+  cidr = "${var.net_address}.0/24"
+  gateway_ip = "${var.net_address}.1"
+  vpc_id = opentelekomcloud_vpc_v1.vpc.id
+  primary_dns = "8.8.8.8"
+  depends_on = [
+    opentelekomcloud_vpc_v1.vpc
+  ]
+}
+
+resource "opentelekomcloud_compute_secgroup_v2" "local_only" {
+  description = "Sec group with only local access"
+  name = "local_only"
+}
