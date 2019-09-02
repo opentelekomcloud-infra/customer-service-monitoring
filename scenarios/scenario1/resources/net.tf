26aaa5bd0969390e16db4724e17f2c5bfca664a2
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2019-09-02 10:12:49 +0300

    subnet name

--- scenarios/scenario1/resources/net.tf
+++ scenarios/scenario1/resources/net.tf
@@ -7,7 +7,7 @@ resource "opentelekomcloud_vpc_v1" "vpc" {
 }
 
 resource "opentelekomcloud_vpc_subnet_v1" "subnet" {
-  name = ""
+  name = "subnet_${var.postfix}"
   cidr = "${var.net_address}.0/24"
   gateway_ip = "${var.net_address}.1"
   vpc_id = opentelekomcloud_vpc_v1.vpc.id
