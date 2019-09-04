6293abd619c5f5f4c9c8859c642170c9ce1555de
Anton Kachurin <anton.kachurin@t-systems.com>
2019-09-04 16:01:40 +0300

    Removed unused file

--- scenarios/scenario2/resources/net.tf
+++ scenarios/scenario2/resources/net.tf
@@ -13,8 +13,3 @@ resource "opentelekomcloud_vpc_subnet_v1" "subnet" {
     opentelekomcloud_vpc_v1.vpc
   ]
 }
-
-resource "opentelekomcloud_compute_secgroup_v2" "local_only" {
-  description = "Sec group with only local access"
-  name = "local_only"
-}
