e7ca7c096291e0682dbcc089c0595001b3b4fc89
Anton Kachurin <anton.kachurin@t-systems.com>
2019-09-05 14:17:58 +0300

    Add venv preparation for test script

--- scenarios/test.sh
+++ scenarios/test.sh
@@ -8,8 +8,19 @@ function get_value() {
 }
 
 project_root=$(bash ./core/get_project_root.sh)
+
+# prepare virtual environment
+poetry --version
+if [[ $? != 0 ]]; then
+    curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
+fi
+cd ${project_root}
+poetry install
+alias python="poetry run python"
+# end venv preparation
+
 scenario_dir="${project_root}/scenarios/${scenario_name}"
 cd ${scenario_dir} || exit 1
 output="$( terraform show | grep "out-" )"
 
-python3 test/main.py
+python test/main.py
