2d0ef5c5a448481edc005ca05eb89d1afc3953b1
Anton Kachurin <anton.kachurin@t-systems.com>
2019-09-06 14:10:56 +0300

    Fix for pre-build ssh agent usage

--- scenarios/scenario2/pre_build.sh
+++ scenarios/scenario2/pre_build.sh
@@ -2,9 +2,9 @@
 
 file_name="scn2_instance_rsa"
 export RSA_PRIVATE_KEY="$( pwd )/${file_name}"
+yes y | ssh-keygen -qf "${RSA_PRIVATE_KEY}" -t rsa -N ''
 ssh-add "${RSA_PRIVATE_KEY}"
 
-yes y | ssh-keygen -qf "${RSA_PRIVATE_KEY}" -t rsa -N ''
 echo "${RSA_PRIVATE_KEY}.pub"
 export TF_VAR_public_key=$( < "${RSA_PRIVATE_KEY}.pub" )
 echo "ECS public key: ${TF_VAR_public_key}"
