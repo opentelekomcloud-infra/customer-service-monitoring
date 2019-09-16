c543bd28dc3658a47abf07424a05691eeb98f884
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2019-09-16 11:55:33 +0300

    fixing issues. minified scenario1_setup

--- scenarios/scenario1/resources/net.tf
+++ scenarios/scenario1/resources/net.tf
@@ -18,7 +18,7 @@ resource "opentelekomcloud_networking_router_v2" "generic" {
 resource "opentelekomcloud_networking_subnet_v2" "subnet" {
   name                = "${var.postfix}_subnet"
   network_id          = opentelekomcloud_networking_network_v2.generic.id
-  cidr                = "${var.net_address}.0/0"
+  cidr                = "${var.net_address}.0/16"
   dns_nameservers     =  ["8.8.8.8", "8.8.8.4"]
 }
 
@@ -31,6 +31,6 @@ resource "opentelekomcloud_networking_router_interface_v2" "http" {
 resource "opentelekomcloud_networking_router_route_v2" "router_route_1" {
   depends_on       = ["opentelekomcloud_networking_router_interface_v2.http"]
   router_id        = opentelekomcloud_networking_router_v2.generic.id
-  destination_cidr = "0.0.0.0/16"
+  destination_cidr = "0.0.0.0/0"
   next_hop         = var.bastion_local_ip
 }
