877ad7924cb2786f31ec1b24fe8a72b7d3e57c2f
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2019-09-23 14:37:27 +0300

    put tls fix to servers config

--- scenarios/scenario1/pre_build.sh
+++ scenarios/scenario1/pre_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ssh-keygen -f "${home}/.ssh/known_hosts" -R csm.outcatcher.com
+ssh-keygen -f ~/.ssh/known_hosts -R csm.outcatcher.com
 file_name="scn1_instance_rsa"
 export RSA_PRIVATE_KEY="$( pwd )/${file_name}"
 ssh-add -d "${RSA_PRIVATE_KEY}"
