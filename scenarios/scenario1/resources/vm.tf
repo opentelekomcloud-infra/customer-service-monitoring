8255798f5fbd62647f9f562f4c4c7716819adc48
Anton Sidelnikov <53078276+anton-sidelnikov@users.noreply.github.com>
2019-09-26 14:40:14 +0300

    Feature/move grafana to new playbook (#45)
    * changes in inventory bastion hostname, moved grafana suite provision to separate playbook
    
    * state parser init commit
    
    * added create inventory script in build.sh

--- scenarios/scenario1/resources/vm.tf
+++ scenarios/scenario1/resources/vm.tf
@@ -17,6 +17,7 @@ resource "opentelekomcloud_compute_instance_v2" "http" {
   network {
     port             = opentelekomcloud_networking_port_v2.http.*.id[count.index]
   }
+  tag                = { "group": "gatewayed" }
 }
 
 # Create network port
