c1215bf3c43acb1794ce30cb4c6825d936f7071f
Anton Kachurin <anton.kachurin@t-systems.com>
2019-09-27 13:23:29 +0300

    Update first boot script for bastion

--- scenarios/modules/bastion/first_boot_bastion.sh
+++ scenarios/modules/bastion/first_boot_bastion.sh
@@ -14,12 +14,13 @@ do
 done < $${file}.old > $${file}
 sudo service sshd restart
 
+mkdir -p /etc/sslcerts/live
+#generate Diffie-Hellman for TLS
+sudo openssl dhparam -out /etc/sslcerts/live/dhparams.pem 2048
+
 #enable port forwarding
 echo 'net.ipv4.ip_forward=1' | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p /etc/sysctl.conf
 
 #configure SNAT
-sudo iptables -t nat -A POSTROUTING -o eth0 -s ${cidr} -j SNAT --to ${bastion_address}
-
-#generate Diffie-Hellman for TLS
-sudo openssl dhparam -out /etc/sslcerts/live/dhparams.pem 2048
+sudo iptables -t nat -A POSTROUTING -o eth0 -s "${cidr}" -j SNAT --to "${bastion_address}"
