deef0b8dcceb7f6d2e524a1078b8be1f8058631f
Anton Kachurin <katchuring@gmail.com>
2019-10-01 10:19:19 +0300

    Initial commit

---
+++ scenarios/scenario1/test/single.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+start_dir=$( pwd )
+local_dir=$( cd $( dirname "$0" ); pwd )
+project_root=$1
+echo "Project root: ${project_root}"
+
+
+scenario_dir=$(cd "${local_dir}/.."; pwd)
+echo "Scenario directory: ${scenario_dir}"
+cd "${scenario_dir}" || exit 1
+terraform init || exit $?
+
+ws_name="single"
+
+terraform workspace select ${ws_name}
+if [[ $? != 0 ]]; then
+    terraform workspace new ${ws_name} || exit $?
+fi
+
+function build() {
+    source ${scenario_dir}/../build.sh scenario1
+}
+
+function destroy() {
+    terraform destroy --auto-approve
+}
+
+function start_stop_rand_node() {
+    if [[ $1 == "stop" ]]; then
+        playbook=playbooks/scenario1_stop_server_on_random_node.yml
+    else
+        playbook=playbooks/scenario1_setup.yml
+    fi
+    cur_dir=$( pwd )
+    cd ${project_root}
+    ansible-playbook -i inventory/prod ${playbook}
+    cd ${cur_dir}
+}
+
+function telegraf_report() {
+    result=$1
+    reason=$2
+    public_ip="$( curl http://ipecho.net/plain -s )"
+    infl_row="lb_down_test,client=${public_ip},reason=${reason} state=${result} $(date +%s%N)"
+    status_code=$( curl -X -o /dev/null -q POST https://csm.outcatcher.com/telegraf -d ${infl_row} -w "%{http_code}" )
+    if [[ status_code != 204 ]]; then
+        echo "Can report status to telegraf"
+        exit 3
+    fi
+}
+
+archive=lb_test.tgz
+file_name=load_balancer_test
+
+wget -O ${archive} https://github.com/opentelekomcloud-infra/csm-test-utils/releases/download/v0.1/lb_test-0.1-linux.tar.gz
+tar xf ${archive}
+
+destroy  # cleanup if previous infra still exists
+build
+./load_balancer_test || not_balanced && exit 1
+
+alias not_balanced="telegraf_report fail \$?"
+
+start_stop_rand_node stop
+./load_balancer_test
+test_result=$?
+if [[ ${test_result} == 0 ]]; then
+    telegraf_report fail multiple_nodes
+    exit ${test_result}
+elif [[ ${test_result} != 101 ]]; then
+    telegraf_report fail ${test_result}
+    exit ${test_result}
+fi
+
+start_stop_rand_node start
+./load_balancer_test || not_balanced && exit 1
+destroy
