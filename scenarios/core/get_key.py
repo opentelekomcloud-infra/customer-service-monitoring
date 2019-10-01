f47153a4f196d62b7580bba7fb5ec4d3c955b410
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2019-10-01 13:40:20 +0300

    modified pre_build and added boto3 in requirements

--- scenarios/core/get_key.py
+++ scenarios/core/get_key.py
@@ -1,25 +1,38 @@
 import os
+from argparse import ArgumentParser
 
 from boto3.session import Session
 from botocore.exceptions import ClientError
 
 api_endpoint = 'https://obs.eu-de.otc.t-systems.com'
-bucket = 'obs-csm'
+bucket = "obs-csm"
 
 
-def copy_key_from_s3():
-    session = Session(aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-                      aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
-    s3 = session.resource('s3', endpoint_url=api_endpoint)
-    try:
-        s3.Bucket(bucket).download_file('key/scn1_instance_rsa',
-                                        'scn1_instance_rsa')
-    except ClientError as e:
-        if e.response['Error']['Code'] == "404":
-            print("The object does not exist.")
-        else:
-            raise
+def parse_params():
+    parser = ArgumentParser('OpenTelekomCloud S3 key downloader')
+    parser.add_argument('--key', '-k', default=None)
+    parser.add_argument('--output', '-o', default=None)
+    args = parser.parse_args()
+    return args
+
+
+class GetKey:
+    def __init__(self):
+        self.args = parse_params()
+        self.copy_key_from_s3()
+
+    def copy_key_from_s3(self):
+        session = Session(aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+                          aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+        s3 = session.resource('s3', endpoint_url=api_endpoint)
+        try:
+            s3.Bucket(bucket).download_file(self.args.key, self.args.output)
+        except ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                print("The object does not exist.")
+            else:
+                raise
 
 
 if __name__ == '__main__':
-    copy_key_from_s3()
+    GetKey()
--- scenarios/scenario1/pre_build.sh
+++ scenarios/scenario1/pre_build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+parent_dir="$(cd "$(dirname "$0")/.." && pwd)"
+
 rm -f  ~/.ssh/known_hosts
 file_name="scn1_instance_rsa"
 export RSA_PRIVATE_KEY="$( pwd )/${file_name}"
 ssh-add -d "${RSA_PRIVATE_KEY}"
-yes y | ssh-keygen -qf "${RSA_PRIVATE_KEY}" -t rsa -N ''
+
+python3 "${parent_dir}/core/get_key.py" --key "key/${file_name}" -o "scn1_instance_rsa"
+sudo chmod 600 ${file_name}
+
 ssh-add "${RSA_PRIVATE_KEY}"
 echo "${RSA_PRIVATE_KEY}.pub"
 export TF_VAR_public_key=$( < "${RSA_PRIVATE_KEY}.pub" )
