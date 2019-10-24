24cb42bcfed4f0f7bacd8a29afbe73393b736d34
Anton Kachurin <anton.kachurin@t-systems.com>
2019-10-24 17:43:39 +0300

    Fixes

--- scenarios/scenario2/test/main.py
+++ scenarios/scenario2/test/main.py
@@ -17,13 +17,13 @@ def check_server(base_url):
 
     data = {"data": _rand_str()}
     response = session.post("/entity", json=data, timeout=2)
-    assert response.status_code == 201, f"Actual status code is {response.status_code}"
-    entity_location = response.headers["Location"]
+    assert response.status_code == 201, f"Actual status code is {response.status_code} ({response.request})"
+    entity_uuid = response.json()["uuid"]
 
-    entity = session.get(entity_location)
+    entity = session.get(f"/entity/{entity_uuid}")
     assert entity.status_code == 200, f"Actual status code is {response.status_code}"
-    assert entity.json() == data
+    assert entity.json()["data"] == data["data"]
 
 
 if __name__ == '__main__':
-    check_server(f"http://{sys.argv[1]}:80/")
+    check_server(f"http://{sys.argv[1]}:80")
