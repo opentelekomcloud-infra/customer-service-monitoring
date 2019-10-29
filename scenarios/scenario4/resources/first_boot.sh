0d8f30f6da86eb45602e694cdc683f4b07eed0f2
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2019-10-29 13:43:50 +0300

    added dashboard

--- scenarios/scenario4/resources/first_boot.sh
+++ scenarios/scenario4/resources/first_boot.sh
@@ -84,6 +84,6 @@ function install_nginx() {
 install_telegraf
 sudo service telegraf restart
 install_nginx
-sudo sed -i '/listen \[::\]:80 default_server;/a add_header Host $hostname;' /etc/nginx/sites-enabled/default
+sudo sed -i '/listen \[::\]:80 default_server;/a add_header Server $hostname;' /etc/nginx/sites-enabled/default
 sudo sed -i '/types_hash_max_size/a more_clear_headers Server;' /etc/nginx/nginx.conf
 sudo service nginx restart
