9a8d47823041752575d84116e5587da6236fa55a
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2019-11-05 13:24:38 +0300

    added become

--- images/scenario4/scripts/provision.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-sudo systemctl enable nginx
-sudo sed -i '/listen \[::\]:80 default_server;/a add_header Server $hostname;' /etc/nginx/sites-enabled/default
-sudo sed -i '/types_hash_max_size/a more_clear_headers Server;' /etc/nginx/nginx.conf
-sudo service nginx restart
