8d79422ec0ed1264d8791d3d1e463aa6fd30d61b
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2019-11-05 12:52:03 +0300

    changed first_boot.sh

--- scenarios/scenario4/resources/first_boot.sh
+++ scenarios/scenario4/resources/first_boot.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-
+sudo systemctl enable nginx
+sudo sed -i '/listen \[::\]:80 default_server;/a add_header Server $hostname;' /etc/nginx/sites-enabled/default
+sudo sed -i '/types_hash_max_size/a more_clear_headers Server;' /etc/nginx/nginx.conf
+sudo service nginx restart
 sudo service telegraf start
-sudo service nginx start
