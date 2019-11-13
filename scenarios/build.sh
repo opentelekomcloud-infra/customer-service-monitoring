3b9a3853b3ac0c865fec2ec6592fb9fe46040d47
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2019-11-13 15:35:14 +0300

    added regexp to generate proper rsa file name

--- scenarios/build.sh
+++ scenarios/build.sh
@@ -17,13 +17,13 @@ export PROJECT_ROOT=$(bash ./core/get_project_root.sh)
 
 # first - build infra
 terraform_dir="${PROJECT_ROOT}/scenarios/${scenario_name}"
-pre_build="./pre_build.sh"
+pre_build="../core/pre_build.sh"
 post_build="./post_build.sh"
 
 
 cd "${terraform_dir}" || exit 1
 terraform init
-if [[ -e ${pre_build} ]]; then source "${pre_build}"; fi
+if [[ -e ${pre_build} ]]; then source "${pre_build}" ${scenario_name}; fi
 terraform apply -auto-approve -input=false "$@" || exit  # pass all scripts arguments to terraform build (for -var support)
 
 # create inventory file after build infrastructure
