8baca64ad6d03cfcf3ce8e8e7708ad85a689a7b0
Anton Kachurin <katchuring@gmail.com>
2019-11-15 18:53:56 +0300

    Move chmod inside of get_key.py

--- scenarios/core/get_key.py
+++ scenarios/core/get_key.py
@@ -3,9 +3,9 @@ from argparse import ArgumentParser
 
 from boto3.session import Session
 from botocore.exceptions import ClientError
+from cryptography.hazmat.backends import default_backend as crypto_default_backend
 from cryptography.hazmat.primitives import serialization as crypto_serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.backends import default_backend as crypto_default_backend
 
 api_endpoint = 'https://obs.eu-de.otc.t-systems.com'
 bucket = 'obs-csm'
@@ -31,21 +31,27 @@ def generate_private_key():
         crypto_serialization.NoEncryption())
 
 
+RO_OWNER = 0o400
+
+
 def copy_key_from_s3():
     args = parse_params()
     session = Session(aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
                       aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
     s3 = session.resource('s3', endpoint_url=api_endpoint)
+    output_file = args.output
     try:
-        s3.Bucket(bucket).download_file(args.key, args.output)
+        s3.Bucket(bucket).download_file(args.key, output_file)
+        os.chmod(output_file, RO_OWNER)
     except ClientError as e:
         if e.response['Error']['Code'] == '404':
             print('The object does not exist in s3. Generating new one ...')
             key = generate_private_key()
             obj = s3.Object(bucket, args.key)
             obj.put(Body=key)
-            with open(args.output, 'wb') as file:
+            with open(output_file, 'wb') as file:
                 file.write(key)
+            os.chmod(output_file, RO_OWNER)
         else:
             raise
 
