72f3f4dd6eac1bf900e607166b72d58642353c83
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2019-11-18 12:56:27 +0300

    changed storage type

--- scenarios/scenario3_5/resources/volumes.tf
+++ scenarios/scenario3_5/resources/volumes.tf
@@ -1,18 +1,14 @@
 # Create volume
-resource "opentelekomcloud_blockstorage_volume_v2" "iSCSI-volume" {
+resource "opentelekomcloud_evs_volume_v3" "iSCSI-volume" {
   name = "scsi-volume"
   size = var.disc_volume
   volume_type = var.volume_type
-  device_type = "SCSI"
+  multiattach = true
+  availability_zone = var.default_az
 }
 
 # Attach volumes to instance
-resource "opentelekomcloud_blockstorage_volume_attach_v2" "attach-iSCSI-volume" {
-  count = var.nodes_count
-  volume_id   = opentelekomcloud_blockstorage_volume_v2.iSCSI-volume.id
-  device     = "auto"
-  host_name  = "basic_${count.index}"
-  ip_address = "${var.net_address}.${count.index + 10}"
-
+resource "opentelekomcloud_compute_volume_attach_v2" "attach-iSCSI-volume" {
+  instance_id = opentelekomcloud_compute_instance_v2.instance.*.id[count.index]
+  volume_id   = opentelekomcloud_evs_volume_v3.iSCSI-volume.id
 }
-
