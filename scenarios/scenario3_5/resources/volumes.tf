2b70423ed3c488ae91484ca05939fbf0391d6074
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2019-11-18 12:57:54 +0300

    added count

--- scenarios/scenario3_5/resources/volumes.tf
+++ scenarios/scenario3_5/resources/volumes.tf
@@ -9,6 +9,7 @@ resource "opentelekomcloud_evs_volume_v3" "iSCSI-volume" {
 
 # Attach volumes to instance
 resource "opentelekomcloud_compute_volume_attach_v2" "attach-iSCSI-volume" {
+  count = var.nodes_count
   instance_id = opentelekomcloud_compute_instance_v2.instance.*.id[count.index]
   volume_id   = opentelekomcloud_evs_volume_v3.iSCSI-volume.id
 }
