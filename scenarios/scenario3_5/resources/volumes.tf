d9fb3f461414c0c4bbc295d1f8b410403432bf4d
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2019-11-18 11:49:43 +0300

    multiattachment

--- scenarios/scenario3_5/resources/volumes.tf
+++ scenarios/scenario3_5/resources/volumes.tf
@@ -1,16 +1,18 @@
 # Create volume
 resource "opentelekomcloud_blockstorage_volume_v2" "iSCSI-volume" {
   name = "scsi-volume"
-  size = 10
+  size = var.disc_volume
   volume_type = var.volume_type
   device_type = "SCSI"
 }
 
 # Attach volumes to instance
 resource "opentelekomcloud_blockstorage_volume_attach_v2" "attach-iSCSI-volume" {
+  count = var.nodes_count
   volume_id   = opentelekomcloud_blockstorage_volume_v2.iSCSI-volume.id
   device     = "auto"
-  host_name  = ""
-  ip_address = ""
+  host_name  = "basic_${count.index}"
+  ip_address = "${var.net_address}.${count.index + 10}"
+
 }
 
