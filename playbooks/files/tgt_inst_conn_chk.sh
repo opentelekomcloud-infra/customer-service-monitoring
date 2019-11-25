8b7ecf78745af2581cdf6b90e7b33f31047c5d29
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2019-11-25 17:24:12 +0300

    Changes in script and setup

--- playbooks/files/tgt_inst_conn_chk.sh
+++ playbooks/files/tgt_inst_conn_chk.sh
@@ -2,9 +2,9 @@
 res="$(tgtadm --mode conn --op show --tid 1)"
 data=$(echo $res | grep -oE "\b[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\b")
 MEASUREMENT="iscsi_connection"
-if [[ -z ${data} ]];
+if [[ ! -z ${data} ]];
 then
-  echo '$MEASUREMENT,tgt_host=$(hostname) status="Connected" address=$data'
+  echo $MEASUREMENT,tgt_host=$(hostname),status=\"Connected\" result=1 address=$data
 else
-  echo '$MEASUREMENT,tgt_host=$(hostname) status="Connection lost"'
+  echo $MEASUREMENT,tgt_host=$(hostname),status=\"Connection lost\" result 0
 fi
