73de8e341cedf7923070c94df883511e22155d8e
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2019-11-25 17:53:58 +0300

    Post result in local telegraf

--- playbooks/files/tgt_inst_conn_chk.sh
+++ playbooks/files/tgt_inst_conn_chk.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
+telegraf="localhost:8080"
 res="$(tgtadm --mode conn --op show --tid 1)"
 data=$(echo $res | grep -oE "\b[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\b")
-MEASUREMENT="iscsi_connection"
 if [[ ! -z ${data} ]];
 then
-  echo $MEASUREMENT,tgt_host=$(hostname),status=\"Connected\" result=1 address=$data
+  telegraf_report connected ok
 else
-  echo $MEASUREMENT,tgt_host=$(hostname),status=\"Connection lost\" result 0
+  telegraf_report connection_lost fail
 fi
+
+function telegraf_report() {
+    result=$1
+    reason=$2
+    MEASUREMENT="iscsi_connection"
+    infl_row="${MEASUREMENT},tgt_host=$(hostname),reason=${reason} state=\"${result}\""
+    status_code=$( curl -q -o /dev/null -X POST ${telegraf} -d "${infl_row}" -w "%{http_code}" )
+    if [[ "${status_code}" != "204" ]]; then
+        echo "Can't report status to telegraf ($status_code)"
+        exit 3
+    fi
+}
