49372675e41c5648cdea94599fd15605255a241b
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2019-11-26 09:34:11 +0300

    moved constant out of func

--- playbooks/files/tgt_inst_conn_chk.sh
+++ playbooks/files/tgt_inst_conn_chk.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 telegraf="localhost:8080/telegraf"
+MEASUREMENT="iscsi_connection"
 session="$(iscsiadm -m session -o show)"
 data=$(echo $session| grep -oE "\b[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\b:[0-9]{4}")
 
 function telegraf_report() {
     result=$1
     reason=$2
-    MEASUREMENT="iscsi_connection"
     infl_row="${MEASUREMENT},tgt_host=$(hostname),reason=${reason} state=\"${result}\""
     status_code=$( curl -q -o /dev/null -X POST ${telegraf} -d "${infl_row}" -w "%{http_code}" )
     if [[ "${status_code}" != "204" ]]; then
