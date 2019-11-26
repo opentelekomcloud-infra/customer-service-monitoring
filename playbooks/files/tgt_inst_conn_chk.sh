3c64068a4585394b26f7dee79a1c1d8371a5314b
Anton Sidelnikov <53078276+anton-sidelnikov@users.noreply.github.com>
2019-11-26 11:40:23 +0300

    Feature/scenario3 5 connection test (#149)
    * initial commit
    
    * fixed copy
    
    * Changes in script and setup
    
    * Post result in local telegraf
    
    * Move func up
    
    * changed url
    
    * moved test to another instance
    
    * moved constant out of func
    
    * proper name to variable

---
+++ playbooks/files/tgt_inst_conn_chk.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+telegraf="localhost:8080/telegraf"
+MEASUREMENT="iscsi_connection"
+session="$(iscsiadm -m session -o show)"
+target_ip=$(echo $session| grep -oE "\b[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\b:[0-9]{4}")
+
+function telegraf_report() {
+    result=$1
+    reason=$2
+    infl_row="${MEASUREMENT},tgt_host=$(hostname),reason=${reason} state=\"${result}\""
+    status_code=$( curl -q -o /dev/null -X POST ${telegraf} -d "${infl_row}" -w "%{http_code}" )
+    if [[ "${status_code}" != "204" ]]; then
+        echo "Can't report status to telegraf ($status_code)"
+        exit 3
+    fi
+}
+
+if [[ ! -z ${target_ip} ]];
+then
+  telegraf_report connected ok
+else
+  telegraf_report connection_lost fail
+fi
