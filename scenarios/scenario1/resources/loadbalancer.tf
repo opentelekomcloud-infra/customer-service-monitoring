6bd31d3958d88795d93d4e86996381bdaa93df37
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-01-10 13:45:05 +0300

    Added prefix for scenario 1 and 1_5

--- scenarios/scenario1/resources/loadbalancer.tf
+++ scenarios/scenario1/resources/loadbalancer.tf
@@ -1,6 +1,6 @@
 # Create loadbalancer
 resource "opentelekomcloud_lb_loadbalancer_v2" "loadbalancer" {
-  name          = "${local.workspace_prefix}elastic_loadbalancer_http"
+  name          = "${local.workspace_prefix}${var.scenario}_elastic_loadbalancer_http"
   vip_subnet_id = var.subnet_id
   vip_address   = var.loadbalancer_local_ip
   depends_on = [
@@ -22,7 +22,7 @@ output "scn1_lb_fip" {
 
 # Create listener
 resource "opentelekomcloud_lb_listener_v2" "listener" {
-  name            = "${local.workspace_prefix}listener_http"
+  name            = "${local.workspace_prefix}${var.scenario}_listener_http"
   protocol        = "TCP"
   protocol_port   = 80
   loadbalancer_id = opentelekomcloud_lb_loadbalancer_v2.loadbalancer.id
@@ -33,7 +33,7 @@ resource "opentelekomcloud_lb_listener_v2" "listener" {
 
 # Set methode for load balance charge between instance
 resource "opentelekomcloud_lb_pool_v2" "pool" {
-  name        = "${local.workspace_prefix}pool_http"
+  name        = "${local.workspace_prefix}${var.scenario}_pool_http"
   protocol    = "TCP"
   lb_method   = "ROUND_ROBIN"
   listener_id = opentelekomcloud_lb_listener_v2.listener.id
@@ -56,7 +56,7 @@ resource "opentelekomcloud_lb_member_v2" "members" {
 
 # Create health monitor for check services instances status
 resource "opentelekomcloud_lb_monitor_v2" "monitor" {
-  name        = "${local.workspace_prefix}monitor_http"
+  name        = "${local.workspace_prefix}${var.scenario}_monitor_http"
   pool_id     = opentelekomcloud_lb_pool_v2.pool.id
   type        = "TCP"
   delay       = 1
