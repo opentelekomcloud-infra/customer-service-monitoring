1a5b0f7f4aa461fef6ab27e985c9321b09a06296
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-01-16 10:41:30 +0300

    vpc in scn2

--- scenarios/scenario2/infrastructure.tf
+++ scenarios/scenario2/infrastructure.tf
@@ -7,11 +7,21 @@ locals {
   }
 }
 
-module "network" {
-  source = "../modules/public_router"
+resource "opentelekomcloud_vpc_v1" "vpc" {
+  name = "${var.prefix}-vpc"
+  cidr = "${var.addr_3_octets}.0/16"
+}
 
-  addr_3_octets = var.addr_3_octets
-  prefix        = "${local.workspace_prefix}${var.scenario}"
+resource "opentelekomcloud_vpc_subnet_v1" "subnet" {
+  name        = "${var.prefix}-subnet"
+  cidr        = "${var.addr_3_octets}.0/24"
+  gateway_ip  = "${var.addr_3_octets}.1"
+  vpc_id      = opentelekomcloud_vpc_v1.vpc.id
+  primary_dns = "8.8.8.8"
+
+  depends_on = [
+    opentelekomcloud_vpc_v1.vpc
+  ]
 }
 
 resource "opentelekomcloud_networking_floatingip_v2" "bastion_public_ip" {}
@@ -23,9 +33,9 @@ module "bastion" {
   ecs_flavor    = var.ecs_flavor
 
   key_pair = local.key_pair
-  network  = module.network.network
-  subnet   = module.network.subnet
-  router   = module.network.router
+  network  = opentelekomcloud_vpc_v1.vpc.id
+  subnet   = opentelekomcloud_vpc_subnet_v1.subnet.id
+  router   = opentelekomcloud_vpc_v1.vpc.id
   name     = "${local.workspace_prefix}bastion"
   scenario = var.scenario
 
@@ -39,9 +49,9 @@ module "postgresql" {
   availability_zone = var.availability_zone
   instance_name     = "scn2-db"
 
-  network_id  = module.network.network.id
-  subnet_id   = module.network.subnet.id
-  subnet_cidr = module.network.subnet.cidr
+  network_id  = opentelekomcloud_vpc_v1.vpc.id
+  subnet_id   = opentelekomcloud_vpc_subnet_v1.subnet.id
+  subnet_cidr = opentelekomcloud_vpc_subnet_v1.subnet.cidr
 
   psql_version  = var.psql_version
   psql_port     = var.psql_port
