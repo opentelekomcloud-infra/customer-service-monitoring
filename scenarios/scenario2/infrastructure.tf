12042711716f4d12965cd47ee9d8c0930d93c965
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-01-16 11:00:18 +0300

    revert changes in scn2

--- scenarios/scenario2/infrastructure.tf
+++ scenarios/scenario2/infrastructure.tf
@@ -1,46 +1,15 @@
+module "resources" {
+  source = "./resources"
 
-locals {
-  workspace_prefix = terraform.workspace == "default" ? "" : "${terraform.workspace}-"
-  key_pair = {
-    public_key = var.public_key
-    key_name   = "${local.workspace_prefix}kp_${var.scenario}"
-  }
-}
+  ecs_image   = var.ecs_image
+  ecs_flavor  = var.ecs_flavor
+  net_address = var.addr_3_octets
+  prefix      = var.prefix
+  public_key  = var.public_key
 
-resource "opentelekomcloud_vpc_v1" "vpc" {
-  name = "${var.prefix}-vpc"
-  cidr = "${var.addr_3_octets}.0/16"
-}
-
-resource "opentelekomcloud_vpc_subnet_v1" "subnet" {
-  name        = "${var.prefix}-subnet"
-  cidr        = "${var.addr_3_octets}.0/24"
-  gateway_ip  = "${var.addr_3_octets}.1"
-  vpc_id      = opentelekomcloud_vpc_v1.vpc.id
-  primary_dns = "8.8.8.8"
-
-  depends_on = [
-    opentelekomcloud_vpc_v1.vpc
-  ]
-}
-
-resource "opentelekomcloud_networking_floatingip_v2" "bastion_public_ip" {}
-
-module "bastion" {
-  source = "../modules/bastion"
-
-  bastion_image = var.ecs_image
-  ecs_flavor    = var.ecs_flavor
-
-  key_pair = local.key_pair
-  network  = opentelekomcloud_vpc_v1.vpc.id
-  subnet   = opentelekomcloud_vpc_subnet_v1.subnet.id
-  router   = opentelekomcloud_vpc_v1.vpc.id
-  name     = "${local.workspace_prefix}bastion"
-  scenario = var.scenario
-
+  region            = var.region
   availability_zone = var.availability_zone
-  bastion_eip       = opentelekomcloud_networking_floatingip_v2.bastion_public_ip.address
+  scenario          = var.scenario
 }
 
 module "postgresql" {
@@ -49,9 +18,9 @@ module "postgresql" {
   availability_zone = var.availability_zone
   instance_name     = "scn2-db"
 
-  network_id  = opentelekomcloud_vpc_v1.vpc.id
-  subnet_id   = opentelekomcloud_vpc_subnet_v1.subnet.id
-  subnet_cidr = opentelekomcloud_vpc_subnet_v1.subnet.cidr
+  network_id  = module.resources.vpc_id
+  subnet_id   = module.resources.subnet.id
+  subnet_cidr = module.resources.subnet.cidr
 
   psql_version  = var.psql_version
   psql_port     = var.psql_port
@@ -59,7 +28,7 @@ module "postgresql" {
 }
 
 output "out-scn2_public_ip" {
-  value = opentelekomcloud_networking_floatingip_v2.bastion_public_ip.address
+  value = module.resources.scn2_eip
 }
 output "out-db_password" {
   value     = module.postgresql.db_password
