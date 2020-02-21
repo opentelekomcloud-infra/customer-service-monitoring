0b52f42f17a242e21adc65d1983a2297774ca904
Anton Sidelnikov <53078276+anton-sidelnikov@users.noreply.github.com>
2020-02-21 12:55:00 +0300

    fixed variables call (#197)

--- install_terraform.sh
+++ install_terraform.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-version=0.12.12
+version=0.12.20
 wget https://releases.hashicorp.com/terraform/${version}/terraform_${version}_linux_amd64.zip
 sudo unzip terraform_${version}_linux_amd64.zip -d /usr/local/bin
