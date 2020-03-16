94e7c3d2e0a045a1cfae17018699c1e5cfff5ad4
Polina-Gubina <Polina.Gubina@t-systems.com>
2020-03-16 17:24:42 +0300

    bashless scenario1_5 and scenario3_5

--- scenarios/scenario3_5/variables.tf
+++ scenarios/scenario3_5/variables.tf
@@ -1,10 +1,5 @@
-variable "username" {}
-variable "password" {}
-variable "region" {}
-variable "tenant_name" {}
 variable "target_availability_zone" {}
 variable "initiator_availability_zone" {}
-variable "domain_name" {}
 variable "ecs_flavor" {}
 variable "ecs_image" {}
 variable "addr_3_octets" { default = "192.168.0" }
