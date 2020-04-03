19fce291b5a661f996331eca02e06b32630de9ba
Polina Gubina <33940358+Polina-Gubina@users.noreply.github.com>
2020-04-03 17:32:22 +0300

    Cleanup csm (#221)
    * Delete redundant files

--- scenarios/scenario3_5/post_build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-output="$(terraform show | grep "out-")"
-
-function get_value() {
-    var_name=$1
-    echo $(echo "${output}" | grep -E "${var_name} =" | grep -oE "\"(.+)\"" | sed -e 's/^"//' -e 's/"$//')
-}
-
-export BASTION_PUBLIC_IP=$(get_value "scn3_5_bastion_fip")
