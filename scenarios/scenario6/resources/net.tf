a5bd73e8db5d02fce2d37b28b9db2723c9d25436
Irina Pereiaslavskaia <Irina.Pereiaslavskaia@t-systems.com>
2020-05-26 16:04:44 +0300

    add scenario6

---
+++ scenarios/scenario6/resources/net.tf
@@ -0,0 +1,29 @@
+#need to combine with scenario2/resources and create module
+#VPC creation
+resource "opentelekomcloud_vpc_v1" "vpc" {
+  name = "${var.prefix}-vpc"
+  cidr = "${var.net_address}.0/16"
+}
+
+#Subnet creation
+resource "opentelekomcloud_vpc_subnet_v1" "subnet" {
+  name        = "${var.prefix}-subnet"
+  cidr        = "${var.net_address}.0/24"
+  gateway_ip  = "${var.net_address}.1"
+  vpc_id      = opentelekomcloud_vpc_v1.vpc.id
+  primary_dns = "8.8.8.8"
+
+  depends_on = [
+    opentelekomcloud_vpc_v1.vpc
+  ]
+}
+
+#Output value for subnet
+output "subnet" {
+  value = opentelekomcloud_vpc_subnet_v1.subnet
+}
+
+#Output value for vpc id
+output "vpc_id" {
+  value = opentelekomcloud_vpc_v1.vpc.id
+}
