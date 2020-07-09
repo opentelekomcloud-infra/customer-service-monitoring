6f6c7acae7ba96dd872121d09a25bd0a8ef23e06
Irina Pereiaslavskaia <Irina.Pereiaslavskaia@t-systems.com>
2020-07-09 10:11:38 +0300

    add main logic to add_records_to_rds.py

--- scenarios/scenario6/settings.tf
+++ scenarios/scenario6/settings.tf
@@ -2,9 +2,19 @@ terraform {
   required_providers {
     opentelekomcloud = ">= 1.16.0"
   }
+  backend "s3" {
+    key      = "terraform_state/scenario2"
+    endpoint = "obs.eu-de.otc.t-systems.com"
+    bucket   = "obs-csm"
+    region   = "eu-de"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
 }
 
 # Configure the OpenTelekomCloud Provider
 provider "opentelekomcloud" {
     cloud = "devstack"
 }
+
