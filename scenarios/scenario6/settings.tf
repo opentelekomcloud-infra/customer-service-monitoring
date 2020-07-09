64fd25388d096d657245929a735c7961cb8c9cb4
Irina Pereiaslavskaia <Irina.Pereiaslavskaia@t-systems.com>
2020-07-09 23:55:49 +0300

    fix settings.tf (scn6)

--- scenarios/scenario6/settings.tf
+++ scenarios/scenario6/settings.tf
@@ -3,7 +3,7 @@ terraform {
     opentelekomcloud = ">= 1.16.0"
   }
   backend "s3" {
-    key      = "terraform_state/scenario2"
+    key      = "terraform_state/scenario6"
     endpoint = "obs.eu-de.otc.t-systems.com"
     bucket   = "obs-csm"
     region   = "eu-de"
