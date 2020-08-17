85239a35c52600fdcca98ce65a7f076ad53eaad5
Polina-Gubina <Polina.Gubina@t-systems.com>
2020-08-18 00:31:29 +0300

    updating submodule

--- playbooks/files/ssh_check_scripts/iperf_client.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-result=$(iperf -c ${1} -b)
-echo $result
-
--- playbooks/files/ssh_check_scripts/iperf_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-iperf -s &
-sleep 10
-pid=$(jobs -l | grep Running | grep iperf -s | awk '{print $2}')
-kill $pid
-exit 0
