3ee2ec19578a183af588d6a56de889b705b340a7
Irina Pereiaslavskaia <Irina.Pereiaslavskaia@t-systems.com>
2020-09-22 11:46:59 +0300

    Fix small issues

--- scenarios/modules/postgresql/network.tf
+++ scenarios/modules/postgresql/network.tf
@@ -1,6 +1,6 @@
 resource "opentelekomcloud_compute_secgroup_v2" "db_local" {
   description = "Sec group with only local access (rds)"
-  name        = "local_only"
+  name        = "local_only_rds"
   rule {
     from_port   = var.psql_port
     ip_protocol = "tcp"
