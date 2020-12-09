d7f3f5e9556f9abad3e62eab7c3ea43bb5e52b8f
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-12-09 14:46:47 +0300

    new folder for scenario

---
+++ scenarios/scsi_hdd_monitoring/settings.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    opentelekomcloud = {
+      source  = "opentelekomcloud/opentelekomcloud"
+      version = ">= 1.22.0"
+    }
+  }
+  backend "s3" {
+    key                         = "terraform_state/scsi_hdd_monitoring"
+    endpoint                    = "obs.eu-de.otc.t-systems.com"
+    bucket                      = "obs-csm"
+    region                      = "eu-de"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
+
+# Configure the OpenTelekomCloud Provider
+provider "opentelekomcloud" {
+  cloud = "devstack"
+}
