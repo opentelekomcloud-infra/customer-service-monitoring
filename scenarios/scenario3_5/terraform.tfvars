e3756db12dfe16d36ef6f64a495827358e61687b
Rodion Gyrbu <fpsoff@outlook.com>
2020-12-11 10:52:36 +0300

    Major changes
    Remove scenarion3_5
    
    Move resources to sfs

--- scenarios/scenario3_5/terraform.tfvars
+++
@@ -1,5 +0,0 @@
-target_availability_zone    = "eu-de-01"
-initiator_availability_zone = "eu-de-03"
-scenario                    = "scn3_5"
-ecs_flavor                  = "s2.large.2"
-ecs_image                   = "Standard_Debian_10_latest"
---
+++ scenarios/sfs_monitoring/variables.tf
@@ -0,0 +1,12 @@
+variable "addr_3_octets" { default = "192.168.0" }
+variable "public_key" { default = "" }
+variable "disc_volume" {
+  type    = number
+  default = 10
+}
+variable "target_availability_zone" { default = "eu-de-01" }
+variable "initiator_availability_zone" { default = "eu-de-03" }
+variable "scenario" { default = "sfs_monitoring" }
+variable "ecs_flavor" { default = "s2.large.2" }
+variable "ecs_image" { default = "Standard_Debian_10_latest" }
+
