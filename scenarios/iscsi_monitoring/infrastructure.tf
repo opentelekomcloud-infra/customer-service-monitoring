63e71d713b215ae4c5b2f79311089fc59faef3b0
Anton Sidelnikov <53078276+anton-sidelnikov@users.noreply.github.com>
2020-12-14 17:57:57 +0300

    Refactoring iscsi scenario infra (#254)
    * move scenario to new folder
    
    * new folder for scenario
    
    * rename folders
    
    * setup stub
    
    * fix modules
    
    * iscsi infra
    
    * endline
    
    * additional vars
    
    * fix variable
    
    * changed eip var name
    
    * infra_state
    
    * set scenario name in playbook
    
    * fix
    
    * added provider
    
    * fixed passing vars to terraform
    
    * key pair
    
    * destroy
    
    * mv kp to vm
    
    * fix typo
    
    * changed addr var

---
+++ scenarios/iscsi_monitoring/infrastructure.tf
@@ -0,0 +1,33 @@
+locals {
+  workspace_prefix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
+  key_pair = {
+    public_key = var.public_key
+    key_name   = "${local.workspace_prefix}_kp_${var.scenario}"
+  }
+}
+
+module "resources" {
+  source = "./resources"
+
+  ecs_image                   = var.ecs_image
+  initiator_availability_zone = var.initiator_availability_zone
+  target_availability_zone    = var.target_availability_zone
+  ecs_flavor                  = var.ecs_flavor
+  disc_volume                 = var.disc_volume
+  key_pair                    = local.key_pair
+
+
+  net_address    = var.addr_3
+  subnet_id      = var.subnet_id
+  network_id     = var.network_id
+  router_id      = var.router_id
+  scenario       = var.scenario
+}
+
+output "iscsi_initiator_instance_ip" {
+  value = module.resources.initiator_instance_ip
+}
+
+output "iscsi_target_instance_ip" {
+  value = module.resources.target_instance_ip
+}
---
+++ scenarios/iscsi_monitoring/resources/variables.tf
@@ -0,0 +1,12 @@
+variable "volume_type" { default = "SATA" }
+variable "key_pair" {}
+variable "ecs_image" {}
+variable "ecs_flavor" {}
+variable "disc_volume" {}
+variable "subnet_id" {}
+variable "network_id" {}
+variable "router_id" {}
+variable "scenario" {}
+variable "target_availability_zone" {}
+variable "initiator_availability_zone" {}
+variable "net_address" {}
