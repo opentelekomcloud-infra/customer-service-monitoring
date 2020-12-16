6038050fd81d8ac4f9fac5677c96c6c6df9cbbb8
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-12-16 15:41:15 +0300

    hdd infra

---
+++ scenarios/hdd_monitoring/infrastructure.tf
@@ -0,0 +1,24 @@
+
+locals {
+  workspace_prefix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
+  key_pair = {
+    public_key = var.public_key
+    key_name   = "${local.workspace_prefix}_kp_${var.scenario}"
+  }
+}
+
+module "nodes" {
+  source               = "./nodes"
+  ecs_flavor           = var.ecs_flavor
+  ecs_image            = var.ecs_image
+  key_pair             = local.key_pair
+  net_address          = var.addr_3
+  network_id           = var.network_id
+  subnet_id            = var.subnet_id
+  vpc_id               = var.router_id
+  scenario             = var.scenario
+}
+
+output "hdd_ecs_local_ips" {
+  value = [ for instance in module.nodes.instances: instance.access_ip_v4 ]
+}
---
+++ scenarios/hdd_monitoring/variables.tf
@@ -0,0 +1,10 @@
+variable "region" { default = "eu-de" }
+variable "availability_zone" { default = "eu-de-03" }
+variable "ecs_flavor" { default = "s2.large.2" }
+variable "ecs_image" { default = "Standard_Debian_10_latest" }
+variable "addr_3" { default = "192.168.0" }
+variable "scenario" { default = "hdd" }
+variable "public_key" {}
+variable "subnet_id" {}
+variable "network_id" {}
+variable "router_id" {}
