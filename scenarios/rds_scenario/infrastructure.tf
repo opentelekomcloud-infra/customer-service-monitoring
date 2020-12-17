d2dd22b0f50110d4e69fc2c84713b9e49c9a62e7
Polina-Gubina <Polina.Gubina@t-systems.com>
2020-12-17 18:48:04 +0300

    Add installing some roles to ecs

--- scenarios/rds_scenario/infrastructure.tf
+++ scenarios/rds_scenario/infrastructure.tf
@@ -69,6 +69,10 @@ resource "opentelekomcloud_compute_instance_v2" "basic" {
   }
 }
 
+output "ecs_ip" {
+  value     = opentelekomcloud_compute_instance_v2.basic.access_ip_v4
+}
+
 output "db_password" {
   value     = module.postgresql.db_password
   sensitive = true
