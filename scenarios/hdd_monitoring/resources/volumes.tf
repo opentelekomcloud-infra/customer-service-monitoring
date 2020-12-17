14f84a6291972c1ea837327f3cba448685474c6d
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-12-17 16:14:39 +0300

    fix volumes attach

--- scenarios/hdd_monitoring/resources/volumes.tf
+++ scenarios/hdd_monitoring/resources/volumes.tf
@@ -8,25 +8,10 @@ resource "opentelekomcloud_blockstorage_volume_v2" "volumes_az1" {
 }
 
 # Attach volumes to instance
-resource "opentelekomcloud_compute_volume_attach_v2" "attach-az1" {
+resource "opentelekomcloud_compute_volume_attach_v2" "attach_az1" {
+  count       = length(opentelekomcloud_blockstorage_volume_v2.volumes_az1)
   instance_id = opentelekomcloud_compute_instance_v2.http[0].id
-  volume_id   = opentelekomcloud_blockstorage_volume_v2.volumes_az1[0].id
-}
-
-resource "opentelekomcloud_compute_volume_attach_v2" "attach-az1-1" {
-  instance_id = opentelekomcloud_compute_instance_v2.http[0].id
-  volume_id   = opentelekomcloud_blockstorage_volume_v2.volumes_az1[1].id
-  depends_on = [
-    opentelekomcloud_compute_volume_attach_v2.attach-az1
-  ]
-}
-
-resource "opentelekomcloud_compute_volume_attach_v2" "attach-az1-2" {
-  instance_id = opentelekomcloud_compute_instance_v2.http[0].id
-  volume_id   = opentelekomcloud_blockstorage_volume_v2.volumes_az1[2].id
-  depends_on = [
-    opentelekomcloud_compute_volume_attach_v2.attach-az1-1
-  ]
+  volume_id   = opentelekomcloud_blockstorage_volume_v2.volumes_az1[count.index].id
 }
 
 resource "opentelekomcloud_blockstorage_volume_v2" "volumes_az2" {
@@ -38,25 +23,10 @@ resource "opentelekomcloud_blockstorage_volume_v2" "volumes_az2" {
 }
 
 # Attach volumes to instance
-resource "opentelekomcloud_compute_volume_attach_v2" "attach-az2" {
+resource "opentelekomcloud_compute_volume_attach_v2" "attach_az2" {
+  count       = length(opentelekomcloud_blockstorage_volume_v2.volumes_az2)
   instance_id = opentelekomcloud_compute_instance_v2.http[1].id
-  volume_id   = opentelekomcloud_blockstorage_volume_v2.volumes_az2[0].id
-}
-
-resource "opentelekomcloud_compute_volume_attach_v2" "attach-az2-1" {
-  instance_id = opentelekomcloud_compute_instance_v2.http[1].id
-  volume_id   = opentelekomcloud_blockstorage_volume_v2.volumes_az2[1].id
-  depends_on = [
-    opentelekomcloud_compute_volume_attach_v2.attach-az2
-  ]
-}
-
-resource "opentelekomcloud_compute_volume_attach_v2" "attach-az2-2" {
-  instance_id = opentelekomcloud_compute_instance_v2.http[1].id
-  volume_id   = opentelekomcloud_blockstorage_volume_v2.volumes_az2[2].id
-  depends_on = [
-    opentelekomcloud_compute_volume_attach_v2.attach-az2-1
-  ]
+  volume_id   = opentelekomcloud_blockstorage_volume_v2.volumes_az2[count.index].id
 }
 
 resource "opentelekomcloud_blockstorage_volume_v2" "volumes_az3" {
@@ -68,23 +38,8 @@ resource "opentelekomcloud_blockstorage_volume_v2" "volumes_az3" {
 }
 
 # Attach volumes to instance
-resource "opentelekomcloud_compute_volume_attach_v2" "attach-az3" {
+resource "opentelekomcloud_compute_volume_attach_v2" "attach_az3" {
+  count       = length(opentelekomcloud_blockstorage_volume_v2.volumes_az3)
   instance_id = opentelekomcloud_compute_instance_v2.http[2].id
-  volume_id   = opentelekomcloud_blockstorage_volume_v2.volumes_az3[0].id
-}
-
-resource "opentelekomcloud_compute_volume_attach_v2" "attach-az3-1" {
-  instance_id = opentelekomcloud_compute_instance_v2.http[2].id
-  volume_id   = opentelekomcloud_blockstorage_volume_v2.volumes_az3[1].id
-  depends_on = [
-    opentelekomcloud_compute_volume_attach_v2.attach-az3
-  ]
-}
-
-resource "opentelekomcloud_compute_volume_attach_v2" "attach-az3-2" {
-  instance_id = opentelekomcloud_compute_instance_v2.http[2].id
-  volume_id   = opentelekomcloud_blockstorage_volume_v2.volumes_az3[2].id
-  depends_on = [
-    opentelekomcloud_compute_volume_attach_v2.attach-az3-1
-  ]
+  volume_id   = opentelekomcloud_blockstorage_volume_v2.volumes_az3[count.index].id
 }
