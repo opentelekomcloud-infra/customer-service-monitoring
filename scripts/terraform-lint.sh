5d05a31d556d8e6a78da5a24eb20d4086c2c13f9
Anton Kachurin <anton.kachurin@t-systems.com>
2020-12-18 18:33:39 +0300

    Fix terraform-lint.sh (#276)
    Fix terraform-lint.sh
    
    Now validate is actually validating
    Now terraform-lint is failing again
    
    Reviewed-by: Anton Sidelnikov <None>
    Reviewed-by: None <None>
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>

--- scripts/terraform-lint.sh
+++ scripts/terraform-lint.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/bash
 terraform fmt -check -recursive ./scenarios/ || exit 1
-find ./scenarios/* -name "scenario*" -exec sh -c 'terraform init "$1" && terraform validate "$1"' _ {} \;
+
+# run tf validate for each scenario
+scenarios=$(find ./scenarios -maxdepth 1)
+status=0
+function check() {
+  echo "Checking $1"
+  terraform init --backend=false --input=false "$1" > /dev/null || return 2
+  echo "Initialized $1"
+  terraform validate "$1" || return 2
+}
+
+for scn in ${scenarios}
+do
+  check "${scn}" || status=2
+done
+
+exit $status
