1ab7086a599ecf3bb8c91aaeac772b39ec7eb50d
Anton Kachurin <anton.kachurin@t-systems.com>
2020-12-21 11:13:16 +0300

    Fix terraform validation in terraform-lint.sh

--- scripts/terraform-lint.sh
+++ scripts/terraform-lint.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/bash
-terraform fmt -check -recursive ./scenarios/ || exit 1
+scenarios_path="$(pwd)/scenarios"
 
-# run tf validate for each scenario
-scenarios=$(find ./scenarios -maxdepth 1)
-status=0
+terraform fmt -check -recursive "$scenarios_path" || exit 1
+
 function check() {
+  cd "$1" || return 2
   echo "Checking $1"
-  terraform init --backend=false --input=false "$1" > /dev/null || return 2
-  echo "Initialized $1"
-  terraform validate "$1" || return 2
+  terraform init --backend=false --input=false > /dev/null || return 2
+  echo "Successfully Initialized"
+  terraform validate || return 2
 }
 
+# run tf validate for each scenario
+scenarios=$(find "$scenarios_path" -maxdepth 1)
+
+status=0
 for scn in ${scenarios}
 do
   check "${scn}" || status=2
