525daa6d4452cd4be28dae8f37b782532d3f88b1
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-12-22 15:41:27 +0300

    added control instance

--- scenarios/lb_monitoring/infrastructure.tf
+++ scenarios/lb_monitoring/infrastructure.tf
@@ -10,18 +10,28 @@ locals {
 module "nodes" {
   source = "../modules/ecs_node"
 
-  ecs_flavor        = var.ecs_flavor
-  ecs_image         = var.ecs_image
-  key_pair_name     = local.key_pair.key_name
-  nodes_count       = var.nodes_count
-  scenario          = var.scenario
-  use_single_az     = var.use_single_az
-
-  net_address          = var.addr_3
-  network_id           = var.network_id
-  subnet_id            = var.subnet_id
+  ecs_flavor    = var.ecs_flavor
+  ecs_image     = var.ecs_image
+  key_pair_name = local.key_pair.key_name
+  nodes_count   = var.nodes_count
+  scenario      = var.scenario
+  use_single_az = var.use_single_az
+  net_address   = var.addr_3
+  network_id    = var.network_id
+  subnet_id     = var.subnet_id
 }
+module "resources" {
+  source = "./resources"
 
+  ecs_image   = var.ecs_image
+  ecs_flavor  = var.ecs_flavor
+  key_pair    = local.key_pair
+  net_address = var.addr_3
+  subnet_id   = var.subnet_id
+  network_id  = var.network_id
+  router_id   = var.router_id
+  scenario    = var.scenario
+}
 module "loadbalancer" {
   source = "../modules/loadbalancer"
 
@@ -38,4 +48,8 @@ output "lb_fip" {
 
 output "lb_ecs_local_ips" {
   value = [for instance in module.nodes.instances : instance.access_ip_v4]
+}
+
+output "lb_ctrl_ip" {
+  value = module.resources.lb_control_instance_ip
 }
---
+++ scenarios/lb_monitoring/resources/variables.tf
@@ -0,0 +1,27 @@
+variable "key_pair" {}
+variable "ecs_image" {}
+variable "ecs_flavor" {
+  default = ""
+}
+variable "disc_volume" {
+  type    = number
+  default = 2
+}
+variable "subnet_id" {
+  default = ""
+}
+variable "network_id" {
+  default = ""
+}
+variable "router_id" {
+  default = ""
+}
+variable "scenario" {
+  default = ""
+}
+variable "availability_zone" {
+  default = "eu-de-01"
+}
+variable "net_address" {
+  default = ""
+}
