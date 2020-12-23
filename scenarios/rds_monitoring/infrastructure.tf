5fab5a7f9c3c73a979fa89a9e978bfecd1e72842
Polina-Gubina <Polina.Gubina@t-systems.com>
2020-12-23 17:13:20 +0300

    Apply terraform format

--- scenarios/rds_monitoring/infrastructure.tf
+++ scenarios/rds_monitoring/infrastructure.tf
@@ -13,7 +13,7 @@ module "postgresql" {
   instance_name     = "${var.scenario}_db_${local.workspace_prefix}"
 
   network_id  = var.network_id
-  router_id = var.router_id
+  router_id   = var.router_id
   subnet_id   = var.network_id
   subnet_cidr = "${var.addr_3}.0/24"
 
@@ -26,20 +26,20 @@ module "resources" {
   source = "./resources"
 
   availability_zone = var.availability_zone
-  key_pair       = local.key_pair
-  ecs_image      = var.ecs_image
-  ecs_flavor     = var.ecs_flavor
-  scenario       = var.scenario
-  region         = var.region
+  key_pair          = local.key_pair
+  ecs_image         = var.ecs_image
+  ecs_flavor        = var.ecs_flavor
+  scenario          = var.scenario
+  region            = var.region
 
-  addr_3    = var.addr_3
-  subnet_id      = var.subnet_id
-  network_id     = var.network_id
-  router_id      = var.router_id
+  addr_3     = var.addr_3
+  subnet_id  = var.subnet_id
+  network_id = var.network_id
+  router_id  = var.router_id
 }
 
 output "ecs_ip" {
-  value     = module.resources.ip
+  value = module.resources.ip
 }
 
 output "db_password" {
