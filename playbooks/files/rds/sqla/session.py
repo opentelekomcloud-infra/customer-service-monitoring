b9a384bf689372bc1159f97837a46e8c9745ec7c
Anton Kachurin <anton.kachurin@t-systems.com>
2020-12-25 19:29:16 +0300

    Fix flake8 errors in `rds` library (#291)
    Fix flake8 errors in `rds` library
    
    Reviewed-by: None <None>
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>

--- playbooks/files/rds/sqla/session.py
+++ playbooks/files/rds/sqla/session.py
@@ -1,17 +1,7 @@
-from sqlalchemy import create_engine, engine
-from sqlalchemy.engine import url
-from sqlalchemy.engine.url import URL
+from sqlalchemy import engine
 from sqlalchemy.orm import sessionmaker
-
-from arg_parser import get_connection_dict
-
-
-def get_db_engine() -> engine:
-    """Create connection to database, and start logging"""
-    return create_engine(url.make_url(URL(**get_connection_dict())), echo = False)
 
 
 def get_session(db_engine: engine):
     """Create session object"""
-    Session = sessionmaker(db_engine)
-    return Session()
+    return sessionmaker(db_engine)()
