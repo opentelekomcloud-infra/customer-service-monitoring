41f5e3f4bb8735a0115a59c645fb56bd1251e70c
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-12-25 18:24:20 +0300

    changed nginx for smn and lookbusy install

--- playbooks/files/autoscaling/install_lookbusy.sh
+++ playbooks/files/autoscaling/install_lookbusy.sh
@@ -1,8 +1,7 @@
-wget http://www.devin.com/lookbusy/download/lookbusy-1.4.tar.gz
-tar -xzf lookbusy-1.4.tar.gz
+wget https://github.com/bugwhine/lookbusy/archive/1.4.tar.gz
+tar -xzf 1.4.tar.gz
 cd lookbusy-1.4
 ./configure
-make
 sudo make install
 cd ../
 rm -rf lookbusy-1.4*
