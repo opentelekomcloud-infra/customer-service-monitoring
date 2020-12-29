15c9e11209f4daace448b5f3b7892628ce61ef48
Anton Kachurin <anton.kachurin@t-systems.com>
2020-12-29 11:41:38 +0300

    Improve infra tests

--- tests/utils.py
+++ tests/utils.py
@@ -1,8 +1,20 @@
 import os
 
-_BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        '..'
+    )
+)
+
+IS_ACCEPTANCE = bool(os.getenv("CSM_ACC"))
+
+
+def project_path(*file_path):
+    """Path starting from project root"""
+    return os.path.join(PROJECT_ROOT, *file_path)
 
 
 def resource_path(*file_path) -> str:
     """Get resource file absolute path for files located in resources dir"""
-    return os.path.join(_BASE_PATH, 'resources', *file_path)
+    return project_path('tests', 'resources', *file_path)
