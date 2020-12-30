3fbccbd59e39f5fe30ead4c5c89b90e4e593955b
Polina-Gubina <Polina.Gubina@t-systems.com>
2020-12-30 13:20:43 +0300

    Remove whitespaces

--- scenarios/rds_backup_monitoring/variables.tf
+++ scenarios/rds_backup_monitoring/variables.tf
@@ -2,7 +2,7 @@ variable "region" { default = "eu-de" }
 variable "availability_zone" {}
 variable "ecs_flavor" { default = "s2.large.2" }
 variable "ecs_image" { default = "Standard_Debian_10_latest" }
-variable "addr_3" { default = "192.168.0" }
+variable "addr_3" {}
 variable "scenario" {}
 variable "psql_version" {}
 variable "psql_port" {}
