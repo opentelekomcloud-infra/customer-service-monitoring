b249fa902055f372ee152080cac0ca27a3d97db5
Anton Kachurin <anton.kachurin@t-systems.com>
2021-01-11 12:38:10 +0300

    Make infra test teardown to work
    Fix tox calling tests

--- tests/acceptance/test_infra.py
+++ tests/acceptance/test_infra.py
@@ -6,6 +6,7 @@ import re
 import subprocess
 import unittest
 
+from roles.build_infrastructure.files.get_key import Credential, acquire_temporary_ak_sk
 from tests.utils import PROJECT_ROOT, project_path
 
 INVENTORY_PATH = project_path('inventory', 'test-infra')
@@ -49,17 +50,20 @@ DEBIAN_IMAGE = 'debian:buster'
 class TestInfrastructure(unittest.TestCase):
     runner = ''
     scenarios: dict = None
+    credential: Credential
 
     @classmethod
     def setUpClass(cls) -> None:
         cls.runner = _get_runner()
         cls.scenarios = _collect_scenarios()
+        cls.credential = acquire_temporary_ak_sk()
         cls.run_playbook('setup_scenarios_controller.yml')
 
     @classmethod
-    def run_playbook(cls, name, **extra_vars):
+    def run_playbook(cls, name, ignore_rc=False, **extra_vars):
         """Run playbook with given tool
 
+        :param bool ignore_rc: Ignore process exit code
         :param str name: Name of playbook to be searched in `$PROJECT_ROOT/playbooks`
         :param extra_vars: Additional variables passed to ansible via `--extra-vars`
         :raises subprocess.CalledProcessError: exception for non-zero exit codes
@@ -72,11 +76,19 @@ class TestInfrastructure(unittest.TestCase):
                 '--inventory', INVENTORY_PATH,
                 '--extra-vars', json.dumps(extra_vars),
                 _playbook_path(name)],
-                check=True,
+                check=not ignore_rc,
                 stderr=subprocess.PIPE,
-                env={'ANSIBLE_NOCOLOR': '1'},
+                stdout=subprocess.DEVNULL,
+                env={
+                    'ANSIBLE_NOCOLOR': '1',
+                    'AWS_ACCESS_KEY_ID': cls.credential.access,
+                    'AWS_SECRET_ACCESS_KEY': cls.credential.secret,
+                    'AWS_SESSION_TOKEN': cls.credential.security_token,
+                    'OS_CLOUD': os.getenv('OS_CLOUD'),
+                },
                 cwd=PROJECT_ROOT,
             )
+            print("Playbook", name, "finished successfully")
         except subprocess.CalledProcessError as cpe:
             logging.error(cpe.stderr)
             raise
@@ -98,7 +110,9 @@ class TestInfrastructure(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        cls.run_playbook('destroy_scenarios_controller.yml')
+        cls.run_playbook('destroy_scenarios_controller.yml', ignore_rc=True)
+        cls.run_playbook('destroy_scenarios_controller.yml', ignore_rc=True)
+        cls.run_playbook('destroy_scenarios_controller.yml', ignore_rc=False)
 
 
 if __name__ == '__main__':
