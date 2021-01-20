d199bf4712bc77a426519598f0fea6ae61ddbc50
Anton Kachurin <anton.kachurin@t-systems.com>
2021-01-20 10:56:46 +0300

    Second part of v2 cleanup (#341)
    * Second part of v2 cleanup
    
    Remove redundant files and playbook contents
    
    Improve build_infrastructure stability
    
    Make build_infrastructure to be less universal
    
    * Fix inventory
    
    Fix test-infra `terraform_workspace` value
    
    Remove unused variables

--- playbooks/files/autoscaling/check_cpu_load_status.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-CPU_LOAD_PID="/tmp/cpu_load.pid"
-if pgrep -F "$CPU_LOAD_PID" >/dev/null
-then
-    echo "cpu_load script is running"
-else
-    start-stop-daemon -Sbmvp /tmp/cpu_load.pid -x /usr/bin/python3 /tmp/cpu_load.py -- --source /tmp/load_levels.dat --interval 60 --ncpus 2
-    echo "cpu_load script is restarted"
-fi
