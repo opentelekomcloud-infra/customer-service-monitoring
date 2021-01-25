71f0b9d2b50dd9b10e440fdb24889d1236dc649b
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2021-01-25 15:42:36 +0300

    shell multiline

--- playbooks/files/rds/check_rds_filling_status.sh
+++ playbooks/files/rds/check_rds_filling_status.sh
@@ -15,15 +15,15 @@ then
     echo "rds filling script is running"
 elif [[ $(grep -c -i "error" ${log_file}) -gt 0 || $(grep -c -i "Script finished" ${log_file}) -eq 0 ]]
 then
-    start-stop-daemon -Sbmvp "$RDS_FILLING_PID" -x /usr/bin/python3 /tmp/rds/__main__.py -- `
-     `--run_option ${run_option} `
-     `--source ${source} `
-     `--host ${pg_host} `
-     `--port ${pg_port} `
-     `--username ${pg_username} `
-     `--password ${pg_password} `
-     `--database ${pg_database} `
-     `--drivername ${drivername}
+    start-stop-daemon -Sbmvp "$RDS_FILLING_PID" -x /usr/bin/python3 /tmp/rds/__main__.py -- \
+    --run_option ${run_option} \
+    --source ${source} \
+    --host ${pg_host} \
+    --port ${pg_port} \
+    --username ${pg_username} \
+    --password ${pg_password} \
+    --database ${pg_database} \
+    --drivername ${drivername}
     echo "rds filling script is restarted"
 else
     echo "rds filling script finished"
