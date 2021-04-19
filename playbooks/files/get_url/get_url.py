e9745dbb2d709867203fbf465da011e7698ef3b3
Anton Sidelnikov <53078276+anton-sidelnikov@users.noreply.github.com>
2021-04-19 21:10:20 +0300

    added snat dnat scripts (#394)
    added snat dnat scripts
    
    Reviewed-by: Anton Kachurin <katchuring@gmail.com>
    Reviewed-by: None <None>
    Reviewed-by: Polina Gubina <None>

---
+++ playbooks/files/get_url/get_url.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from time import sleep
+
+import requests
+import statsd
+import yaml
+
+
+def main():
+    with open('get_url.yaml') as file:
+        args = yaml.safe_load(file)
+    client = statsd.StatsClient(args["host"], args["port"])
+    while True:
+        for host in args["nodes"]:
+            metric_name = f'{args["statsd_prefix"]}.{args["metric_name"]}.{host["name"]}'
+            try:
+                res = requests.get(host["ip"], headers={'Connection': 'close'}, timeout=5)
+                client.timing(f'{metric_name}', int(res.elapsed.total_seconds() * 1000))
+            except Exception as ex:
+                client.incr(f'counters.{metric_name}.failed')
+                print(f'{host["name"]} caused {ex} by invalid response')
+        sleep(10)
+
+
+if __name__ == '__main__':
+    main()
