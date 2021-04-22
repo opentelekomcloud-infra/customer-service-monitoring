1fc67134c1bb9504584b7719ee7159a062ee0ef4
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2021-04-22 18:21:36 +0300

    double assigment fix

--- playbooks/files/metadata/get_metadata.py
+++ playbooks/files/metadata/get_metadata.py
@@ -13,16 +13,15 @@ def main():
     with open('get_metadata.yaml') as file:
         args = yaml.safe_load(file)
     client = statsd.StatsClient(args["host"], args["port"])
+    metric_name = f'{args["statsd_prefix"]}.{args["metric_name"]}.{socket.gethostname()}'
     while True:
-        metric_name = args["metric_name"]
-        metric_name = f'{args["statsd_prefix"]}.{args["metric_name"]}.{socket.gethostname()}'
         try:
             res = requests.get(METADATA_URL, headers={'Connection': 'close'}, timeout=5)
             client.timing(f'{metric_name}.{res.status_code}', res.elapsed.total_seconds() * 1000)
         except Exception as ex:
             client.incr(f'counters.{metric_name}.failed', 10)
             print(f'{socket.gethostname()} caused {ex} by invalid response')
-        sleep(60)
+        sleep(1)
 
 
 if __name__ == '__main__':
